7c8ba1a330e6db40bc6cc1f404471940b3ad32be
Liz Hall <lizk.hall@gmail.com>
2023-12-05 21:15:26 -0600

    edited error message feature

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-    let errorMessage = "An unknown error occurred";
+    let errorMessage = "An unknown error occurred. Check console log for more information.";
     let statusCode = 500;
         if (isHttpError(error)) {
             statusCode = error.status;
